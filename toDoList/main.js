e724907129bd1d7ddece2ba772eb780ad400626c
Anikó Borosová <borosanikoo@gmail.com>
2018-09-10 12:05:43 +0200

    vuetify, methods clarified

--- toDoList/main.js
+++ toDoList/main.js
@@ -12,28 +12,43 @@ const app = new Vue({
 	el: "#app",
 	data: {
 		userInput: userInputValue,
-		todos: todoArray,
-		//done: false,
-		checked: false,
-		visible: true
+		todos: todoArray//,
+		//checked: false,
+		//visible: true
 	},
 	methods: {
 		addTask: function() {
-			this.todos.push(this.userInput);
+			//this.todos.push(this.userInput);
+			this.todos.push({
+				checked: false,
+				visible: true,
+				todoText: this.userInput
+			});
 			this.userInput = "";
 			return;
 		},
-		/*taskDone: function() {
-			this.done = true;
-		},*/
-		deleteTask: function() {
-			this.visible = false;
+		deleteTask: function(index) {
+			//this.visible = false;
+			this.todos.splice(index, 1);
 		}
 	}
 });
 
+window.addEventListener("keypress", function(event) {
+	if (event.key === "Enter") {
+		app.addtask();
+		//console.log(this);
+	}
+});
 
 // things to work out
 // 1. cross only the current task
 // 2. delete only the current task
 // 3. work it out without this???
+// 4. call a function from another vue function
+
+// v-input does not work
+// colors like primary, secondary does not work
+
+// vue.js file
+// unit test
